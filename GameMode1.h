9816969e8b1e4e844ad8643996fec8a6d1c6b2df
hhhhyelim <110897810+hhhhyelim@users.noreply.github.com>
2023-05-12 16:01:49 +0900

    20230512

--- GameMode1.h
+++ GameMode1.h
@@ -7,11 +7,13 @@ class Mode1 : public PhaseInterface
 public:
 	Mode1();
 	~Mode1();
+
 	virtual void HandleEvents();
 	virtual void Update();
 	virtual void Render();
+	void userMove();
+	void holdMove();
 
 private:
 
 };
-
